d26b906016eaefbcdd9f52d6191cf9f7352dc764
fchristian1 <fchristian1@gmx.net>
2024-08-04 14:28:25 +0200

    20240804-142825

---
+++ 20240803/server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const fs = require("node:fs");
+
+const app = express();
+app.use(express.json());
+
+let counter = 0;
+let url = "example.de/api/v1/data";
+
+app.get("/", (req, res) => {
+  const data = fs.readFileSync("index.html", "utf8");
+  counter += 1;
+  console.log(counter);
+  res.send(data);
+});
+
+app.get("/secondpage.html", (req, res) => {
+  const data = fs.readFileSync("secondpage.html", "utf8");
+  counter += 1;
+  console.log(counter);
+  res.send(data);
+});
+
+app.listen(3003, () => {
+  console.log("is listen on port 3003!");
+});
