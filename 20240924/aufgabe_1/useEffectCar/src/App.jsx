a362cf83bf5435c6e8c8549345af1c2ae7e442db
fchristian1 <fchristian1@gmx.net>
2024-09-25 08:52:41 +0200

    20240925-085241

--- 20240924/aufgabe_1/useEffectCar/src/App.jsx
+++ 20240924/aufgabe_1/useEffectCar/src/App.jsx
@@ -7,16 +7,10 @@ function App() {
     return (
         <>
             <h1>CarsViewApp Aufgabe 1 24.09.2024</h1>
-            <button
-                className="bg-gray-400 rounded p-1 border border-gray-600 m-1 hover:bg-gray-500"
-                onClick={() => setMenu(0)}
-            >
+            <button className="button-primary" onClick={() => setMenu(0)}>
                 CarsListView
             </button>
-            <button
-                className="bg-gray-400 rounded p-1 border border-gray-600 m-1 hover:bg-gray-500"
-                onClick={() => setMenu(1)}
-            >
+            <button className="button-primary" onClick={() => setMenu(1)}>
                 CarsListViewWithTimer
             </button>
             {menu == 0 ? <CarsListView /> : <CarsListViewWithTimer />}
