f4789cf865df35492c49e1c75cda63e622dce3ea
fchristian1 <fchristian1@gmx.net>
2024-11-06 21:53:29 +0100

    20241106-215329

---
+++ 20241106/hausi_1/README.md
@@ -0,0 +1,15 @@
+hausaufgaben projekt vom 06.11.2024
+
+backend mit node.js und express.js
+
+zusätzliches frontend mit react.js
+
+ausführung:
+
+``` bash
+git clone https://github.com/fchristian1/techstarter-class.git
+
+cd techstarter-class/20241106/hausi_1
+
+./start
+```
--- 20241106/hausi_1/frontend/src/main.jsx
+++ 20241106/hausi_1/frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ItemsProvider } from "./services/Items.service";
+import { ItemsProvider } from "./services/Items.service.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
